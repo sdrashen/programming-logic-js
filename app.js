8d719ed544b53a57e31d76d2e83b10ed465aa2b7
drashen <sdrashe@gmail.com>
2025-02-18 15:17:45 -0300

    feat: use ternary operator for conditional logic

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ while (chute != numeroSecreto) {
   chute = prompt('Escolha um número entre 1 e 10');
   //se o chute for igual ao número secreto
   if (chute == numeroSecreto) {
-    alert(`Acertou!! O num é ${numeroSecreto} com ${tentativas} tentativas!`);
+    break;
   } else {
     if (chute > numeroSecreto) {
       alert(`O número secreto é menor que ${chute}!`);
@@ -19,3 +19,12 @@ while (chute != numeroSecreto) {
     tentativas++;
   }
 }
+
+
+let palavraTentativa = tentativas > 1 ? 'tentativas' : 'tentativa';
+alert(`Isso aí! Acertou o número secreto ${numeroSecreto} com ${tentativas}  ${palavraTentativa}.`);
+//if (tentativas > 1) {
+//  alert(`Você acertou em ${tentativas} tentativas! O número secreto era ${numeroSecreto}.`);
+//} else {
+//  alert(`Você acertou com ${tentativas} tentativa! O número secreto era ${numeroSecreto}.`);
+//}
