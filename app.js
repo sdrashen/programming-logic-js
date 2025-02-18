29af059765a3afe2465bde9fa35caf7fc2915f57
drashen <sdrashe@gmail.com>
2025-02-18 13:25:04 -0300

    feat: add while loop implementation

--- app.js
+++ app.js
@@ -1,11 +1,21 @@
 alert('Boas vindas ao jogo do número secreto!');
-let numeroSecreto= 5;
+let numeroSecreto = 5;
 console.log(numeroSecreto);
-let chute = prompt('Escolha um número entre 1 e 10');
+let chute;
+let tentativas = 1;
 
-//se o chute foir igual ao número secreto
-if (chute == numeroSecreto) {
-  alert(`Acertou!! O num é ${numeroSecreto}!`);
-} else {
-  alert('Errou!')
+//enquanto chute não for o certo
+while (chute != numeroSecreto) {
+  chute = prompt('Escolha um número entre 1 e 10');
+  //se o chute for igual ao número secreto
+  if (chute == numeroSecreto) {
+    alert(`Acertou!! O num é ${numeroSecreto} com ${tentativas} tentativas!`);
+  } else {
+    if (chute > numeroSecreto) {
+      alert(`O número secreto é menor que ${chute}!`);
+    } else {
+      alert(`O número secreto é maior que ${chute}!`);
+    }
+    tentativas++;
+  }
 }
